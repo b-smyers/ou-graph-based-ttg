830f13a3762cbc583da6de88e903565c09b89046
Brendan Smyers <bs602422@ohio.edu>
2026-02-02 15:10:41 -0500

    Use logger

--- load.py
+++ load.py
@@ -1,12 +1,13 @@
+# load.py: Clears the database before loading the data from the provided <catalog-path> into the database
 from dotenv import load_dotenv
 import os
 import sys
 from neo4j import GraphDatabase
 import uuid
 import json
+from logger import logger
 
 load_dotenv()
-# load.py: Clears the database before loading the data from the provided <catalog-path> into the database
 
 # Config
 CATALOG_PATH = sys.argv[1] if len(sys.argv) >= 2 else None
@@ -17,10 +18,10 @@ if not CATALOG_PATH:
     print("Usage: load.py <catalog-path>")
     exit()
 if not URI:
-    print("[ ERROR ]: Missing NEO4J_DB_URI. Did you set the env?")
+    logger.error("Missing NEO4J_DB_URI. Did you set the env?")
     exit()
 if not AUTH[0] or not AUTH[1]:
-    print("[ ERROR ]: Missing NEO4J_USERNAME or NEO4J_PASSWORD. Did you set the env?")
+    logger.error("Missing NEO4J_USERNAME or NEO4J_PASSWORD. Did you set the env?")
     exit()
 
 driver = GraphDatabase.driver(URI, auth=AUTH)  # type: ignore
@@ -101,8 +102,8 @@ def create_placement(tx, subject, level):
 def create_level(tx, level):
     level_to_credits = {"freshman": 0, "sophomore": 30, "junior": 60, "senior": 90}
     if level not in level_to_credits:
-        print(
-            f"[ ERROR ]: '{level}' did not match any level options: {', '.join(list(level_to_credits.keys()))}"
+        logger.error(
+            f"'{level}' did not match any level options: {', '.join(list(level_to_credits.keys()))}"
         )
         return None
 
@@ -266,7 +267,7 @@ def process_course_requisites(tx, course):
     # Find the course by its code to get its UUID
     node = find_course_by_code(tx, code=course["code"])
     if node is None:
-        print("[ERROR] Could not find previously imported course.")
+        logger.error("Could not find previously imported course.")
         return  # Course not found (should not happen)
     course_uuid = node["uuid"]
 
@@ -289,7 +290,7 @@ def process_requisite(tx, parent_req, parent_uuid):
             permission_uuid = create_permission(tx, permission_authority)
             create_requires(tx, parent_uuid, permission_uuid)
         else:
-            print("[ERROR] 'authority' property was expected, but was not found")
+            logger.error("'authority' property was expected, but was not found")
 
     elif t == "GPA":
         gpa = parent_req.get("gpa", None)
@@ -297,15 +298,15 @@ def process_requisite(tx, parent_req, parent_uuid):
             gpa_uuid = create_gpa(tx, gpa)
             create_requires(tx, parent_uuid, gpa_uuid)
         else:
-            print("[ERROR] 'gpa' property was expected, but was not found")
+            logger.error("'gpa' property was expected, but was not found")
 
     elif t == "COURSE":
         course_code = parent_req.get("course", None)
         if course_code is not None:
             node = find_course_by_code(tx, course_code)
             if node is None:
-                print(
-                    f"[WARN] Could not find requisite course with course code '{course_code}'."
+                logger.warn(
+                    f"Could not find requisite course with course code '{course_code}'."
                 )
                 return
 
@@ -318,16 +319,16 @@ def process_requisite(tx, parent_req, parent_uuid):
                 create_concurrent(tx, parent_uuid, node["uuid"])
                 create_requires(tx, parent_uuid, node["uuid"])
         else:
-            print("[ERROR] 'course' property was expected, but was not found")
+            logger.error("'course' property was expected, but was not found")
 
     elif t == "PLACEMENT":
         placement_subject = parent_req.get("subject", None)
         placement_level = parent_req.get("level", None)
         if placement_subject is None:
-            print("[ERROR] 'placement' property was expected, but was not found")
+            logger.error("'placement' property was expected, but was not found")
             return
         if placement_level is None:
-            print("[ERROR] 'level' property was expected, but was not found")
+            logger.error("'level' property was expected, but was not found")
             return
 
         placement_uuid = create_placement(tx, placement_subject, placement_level)
@@ -339,7 +340,7 @@ def process_requisite(tx, parent_req, parent_uuid):
             level_uuid = create_level(tx, level_name)
             create_requires(tx, parent_uuid, level_uuid)
         else:
-            print("[ERROR] 'level' property was expected, but was not found")
+            logger.error("'level' property was expected, but was not found")
 
     elif t in ("AND", "OR"):
         requirements = parent_req.get("requirements", None)
@@ -350,7 +351,7 @@ def process_requisite(tx, parent_req, parent_uuid):
             for c in requirements:
                 process_requisite(tx, c, parent_uuid=reqgroup_uuid)
         else:
-            print("[ERROR] 'requirements' property was expected, but was not found")
+            logger.error("'requirements' property was expected, but was not found")
 
     elif t == "OTHER":
         other = parent_req.get("other", None)
@@ -358,7 +359,7 @@ def process_requisite(tx, parent_req, parent_uuid):
             other_uuid = create_other(tx, other)
             create_requires(tx, parent_uuid, other_uuid)
         else:
-            print("[ERROR] 'other' property was expected, but was not found")
+            logger.error("'other' property was expected, but was not found")
 
     else:
         raise ValueError(
@@ -368,30 +369,30 @@ def process_requisite(tx, parent_req, parent_uuid):
 
 def main():
     with driver.session() as session:
-        print("[INFO] Cleared DB.")
+        logger.info("Cleared DB.")
         clear_db(session)
 
         total_courses = len(catalog)
 
-        print(f"[INFO] Creating {total_courses} course nodes.")
+        logger.info(f"Creating {total_courses} course nodes.")
         # First pass: Create all course nodes
         for i, course in enumerate(catalog):
             if i % 100 == 0:
-                print(
-                    f"[INFO] Creating courses {i}/{total_courses} - {round(100 * i / total_courses, 2)}%"
+                logger.info(
+                    f"Creating courses {i}/{total_courses} - {round(100 * i / total_courses, 2)}%"
                 )
             session.execute_write(create_course, course)
 
-        print("[INFO] Creating requisites relationships.")
+        logger.info("Creating requisites relationships.")
         # Second pass: Process requisites
         for i, course in enumerate(catalog):
             if i % 100 == 0:
-                print(
-                    f"[INFO] Processing course requisites {i}/{total_courses} - {round(100 * i / total_courses, 2)}%"
+                logger.info(
+                    f"Processing course requisites {i}/{total_courses} - {round(100 * i / total_courses, 2)}%"
                 )
             session.execute_write(process_course_requisites, course)
 
-    print("[INFO] Catalog loaded into Neo4j.")
+    logger.info("Catalog loaded into Neo4j.")
 
 
 if __name__ == "__main__":
